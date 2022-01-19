cf498c09efe1c575e1ef9f932fb81e447c4ccf7b
Lilly Daniell <daniell.lilly@gmail.com>
2022-01-19 21:23:15 +0000

    Add .run_app class method

--- lib/echo.rb
+++ lib/echo.rb
@@ -1,26 +1,31 @@
-def prompt
-  puts "Say something:"
-end
-
-def generate_echo(user_input)
-  if user_input == "exit"
-    puts "Goodbye!"
-  else 
-    puts "#{date} | #{time} | You said: '#{user_input}'!"
+class Echo
+  def self.run_app
+    echo = Echo.new
+    while true do
+      echo.prompt
+      user_input = gets.chomp 
+      echo.generate_echo(user_input)
+      break if user_input == 'exit'
+    end
   end
-end
 
-def date
-  "#{Time.now.strftime("%F")}"
-end
+  def prompt
+    puts "Say something:"
+  end
 
-def time
-  "#{Time.now.strftime("%H:%M")}"
-end
+  def generate_echo(user_input)
+    if user_input == "exit"
+      puts "Goodbye!"
+    else 
+      puts "#{date} | #{time} | You said: '#{user_input}'!"
+    end
+  end
 
-while true do
-  prompt
-  user_input = gets.chomp 
-  generate_echo(user_input)
-  break if user_input == 'exit'
+  def date
+    "#{Time.now.strftime("%F")}"
+  end
+
+  def time
+    "#{Time.now.strftime("%H:%M")}"
+  end
 end
--- spec/echo_spec.rb
+++ spec/echo_spec.rb
@@ -1,18 +1,23 @@
 require 'echo'
 require 'timecop'
 
-describe 'Echo-app' do
- 
+describe Echo do
+  let(:echo) { Echo.new }
+
   it "should begin with a prompt" do
-    expect { prompt }.to output("Say something:\n").to_stdout
+    expect { echo.prompt }.to output("Say something:\n").to_stdout
   end
 
   it "should output 'Goodbye!' if user inputs 'exit'" do
-    expect { generate_echo("exit") }.to output("Goodbye!\n").to_stdout
+    expect { echo.generate_echo("exit") }.to output("Goodbye!\n").to_stdout
   end
 
   it "should create an echo summary from user input" do
     Timecop.freeze(Time.local(2018, 1, 9, 16, 26))
-    expect { generate_echo("hello, world") }.to output("2018-01-09 | 16:26 | You said: 'hello, world'!\n").to_stdout
+    expect { echo.generate_echo("hello, world") }.to output("2018-01-09 | 16:26 | You said: 'hello, world'!\n").to_stdout
+  end
+
+  it ".run_app should run the program" do
+    expect(Echo.run_app).to eq(echo.prompt)
   end
 end
